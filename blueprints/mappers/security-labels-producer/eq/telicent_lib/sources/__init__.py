15c000d7722c90700d744ed26515abf07a9b1073
Hariharan Ms <hariharan.ms@neso.energy.com>
2026-03-24 20:24:08 +0530

    telicent lib source code removed

--- blueprints/mappers/security-labels-producer/eq/telicent_lib/sources/__init__.py
+++
@@ -1,5 +0,0 @@
-from telicent_lib.sources.dataSource import DataSource
-from telicent_lib.sources.deserializers import DeserializerFunction, Deserializers, RdfDeserializer
-from telicent_lib.sources.kafkaSource import KafkaSource
-
-__all__ = ['DeserializerFunction', 'Deserializers', 'RdfDeserializer', 'DataSource', 'KafkaSource']
