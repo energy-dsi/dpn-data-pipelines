902678eac656e7dc5f1716b0a7bc346e2952466c
Hariharan Ms [MSP] <hariharan.ms@neso.energy>
2026-04-20 16:02:18 +0530

    exception handling added

--- blueprints/consumer/file/schema_mapper/main.py
+++ blueprints/consumer/file/schema_mapper/main.py
@@ -47,11 +47,10 @@ class SchemaMapper:
 
         self.logger.info("cloudProviderType : %s ", self.cloud_provider)
         self.logger.info("mapperTopicName : %s", self.target_kafka_topic)
-        self.logger.info("srcConnectionString : %s", self.source_azure_conn_str)
+
         self.logger.info("srcContainerName : %s", self.source_container_name)
         self.logger.info("mapperContainerName : %s", self.target_container_name)
         self.logger.info("bootstrapServer : %s", self.boostrap_server)
-        self.logger.info("mapperConnectionString : %s", self.target_azure_conn_str)
 
     def read_from_kafka_topic(self):
         """
